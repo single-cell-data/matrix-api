7e464e2676a1f124d6dfb44109ef8c33c980024a
Ryan Williams <ryan.williams@tiledb.com>
2024-11-07 21:25:59 -0500

    export `query.Experimentish`
    for use by `tiledbsoma._experiment.Experiment`

--- python-spec/src/somacore/query/__init__.py
+++ python-spec/src/somacore/query/__init__.py
@@ -2,12 +2,14 @@ from . import axis
 from . import query
 
 ExperimentAxisQuery = query.ExperimentAxisQuery
+Experimentish = query.Experimentish
 AxisColumnNames = query.AxisColumnNames
 AxisIndexer = query.AxisIndexer
 AxisQuery = axis.AxisQuery
 
 __all__ = (
     "ExperimentAxisQuery",
+    "Experimentish",
     "AxisColumnNames",
     "AxisIndexer",
     "AxisQuery",
